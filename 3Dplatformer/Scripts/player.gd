58421a76e9e3bd614d1ad5cb61b63132e4377b34
Juan Francisco Guarracino <jfguarracino@gmail.com>
2026-08-27 20:00:45 -0300

    3d platformer
    - finished the moving of the character

--- 3Dplatformer/Scripts/player.gd
+++ 3Dplatformer/Scripts/player.gd
@@ -1,28 +1,24 @@
 extends CharacterBody3D
 
+@export var move_speed : float = 3.0
+@export var jump_force : float = 8
+@export var gravity : float = 20
 
-const SPEED = 5.0
-const JUMP_VELOCITY = 4.5
-
+@onready var camera : Camera3D = $Camera3D
 
 func _physics_process(delta: float) -> void:
-	# Add the gravity.
-	if not is_on_floor():
-		velocity += get_gravity() * delta
-
-	# Handle jump.
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
-		velocity.y = JUMP_VELOCITY
-
-	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
-	var input_dir := Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down")
-	var direction := (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
-	if direction:
-		velocity.x = direction.x * SPEED
-		velocity.z = direction.z * SPEED
-	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
-		velocity.z = move_toward(velocity.z, 0, SPEED)
-
+	# gravity
+	velocity.y -= gravity * delta
+	
+	# jump
+	if Input.is_action_pressed('jump')and is_on_floor():
+		velocity.y = jump_force
+	
+	# movement
+	var move_input : Vector2 = Input.get_vector('move_left', 'move_right', 'move_forward', "move_backwards")
+	var move_dir : Vector3 = Vector3(move_input.x, 0, move_input.y)
+	
+	velocity.x = move_dir.x * move_speed
+	velocity.z = move_dir.z * move_speed
+	
 	move_and_slide()
